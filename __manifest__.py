ad932a3e5a9ce45014b9e14ac50b16efbcf77c7f
bengin <baris.engin@gmail.com>
2026-05-21 17:30:42 +0000

    fix

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'MS File Viewer Extension',
     'summary': 'Enhances file viewing capabilities in Odoo',
+    'author': 'Biglogic',
     'category': 'Biglogic',
     'depends': ['base', 'web', 'mail'],
     'data': [],
